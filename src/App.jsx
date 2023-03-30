035f60b602ed4ce46b31b45d5ed5197b7d712457
audricdet <audric.det@hotmail.com>
2023-03-30 11:59:49 +0200

    Vite react and tailwind architecture

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from "react"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <p>Super application de kevin</p>
+    </div>
+  )
+}
+
+export default App
