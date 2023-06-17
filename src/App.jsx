82c1812e35a136995b44754b589789ee736e0684
audricdet <audric.det@hotmail.com>
2023-06-17 11:19:58 +0200

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import ProfilePage from './pages/ProfilePage';
 import CompleteProfilePage from './pages/CompleteProfilePage';
 import EditProfileInfos from './pages/EditProfilePage';
 import CategoriesList from './pages/CategoriesList';
+import CategoryAssociations from './pages/CategoryAssociations';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,7 +23,6 @@ function App() {
       <AuthProvider>
         <BrowserRouter history={history}>
           <Routes>
-            <Route path="/" element={<LoginPage />}/>
             <Route path="/login" element={<LoginPage />} /> 
             <Route path="/register" element={<RegisterPage/>}/>
             <Route path="/associations" element={<AssociationsPage/>}/>
